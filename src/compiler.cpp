4008a7dcbbabe1c10ef4af40550f18f5ae4f5210
Roman Sokolkov <roman.sokolkov@apex.ai>
2022-05-08 16:58:51 +0200

    Use llc not finished

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -1,92 +1,81 @@
+#include <codecvt>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
 
+#include <llvm/IR/BasicBlock.h>
+#include <llvm/IR/DerivedTypes.h>
 #include <llvm/IR/Function.h>
 #include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/LegacyPassManager.h>
 #include <llvm/IR/Module.h>
 #include <llvm/IR/Type.h>
-#include <llvm/Support/TargetRegistry.h>
-#include <llvm/Target/TargetOptions.h>
-#include <llvm/IR/BasicBlock.h>
-#include <llvm/IR/DerivedTypes.h>
-#include <llvm/IR/LegacyPassManager.h>
 #include <llvm/Support/FileSystem.h>
 #include <llvm/Support/Host.h>
+#include <llvm/Support/TargetRegistry.h>
 #include <llvm/Support/TargetSelect.h>
 #include <llvm/Support/raw_ostream.h>
 #include <llvm/Target/TargetMachine.h>
+#include <llvm/Target/TargetOptions.h>
+
+#include "codegen.hpp"
+#include "parser.hpp"
+#include "tokenizer.hpp"
+
+namespace fs = std::filesystem;
+
+std::string readFileIntoString(const std::string &path) {
+  std::ifstream input_file(path);
+  if (!input_file.is_open()) {
+    std::cerr << "Could not open the file - '" << path << "'" << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
+  return std::string((std::istreambuf_iterator<char>(input_file)),
+                     std::istreambuf_iterator<char>());
+}
 
 int main(int argc, char **argv) {
   if (argc != 2) {
     std::cerr << "Usage: unzig <file>.uz" << std::endl;
     std::exit(1);
   }
-  std::filesystem::path inputFile = argv[1];
-
-  // TODO: read input file
-
-  llvm::LLVMContext llvmCtxt;
-  llvm::Module llvmModule("foo", llvmCtxt);
-  llvm::IRBuilder<> llvmIRBuilder(llvmCtxt);
-
-  // empty main
-  auto *funcType = llvm::FunctionType::get(llvmIRBuilder.getInt32Ty(), false);
-  auto *mainFunc = llvm::Function::Create(
-      funcType, llvm::Function::ExternalLinkage, "main", llvmModule);
-  auto *entry = llvm::BasicBlock::Create(llvmCtxt, "entrypoint", mainFunc);
-  llvmIRBuilder.SetInsertPoint(entry);
-  llvmIRBuilder.CreateRet(llvmIRBuilder.getInt32(0));
-
-  // print LLVM IR
-  // llvmModule.print(llvm::outs(), nullptr);
-
-  // Initialize the target registry etc.
-  llvm::InitializeAllTargetInfos();
-  llvm::InitializeAllTargets();
-  llvm::InitializeAllTargetMCs();
-  llvm::InitializeAllAsmParsers();
-  llvm::InitializeAllAsmPrinters();
-  auto targetTriple = llvm::sys::getDefaultTargetTriple();
-  llvmModule.setTargetTriple(targetTriple);
-  std::string error;
-  auto target = llvm::TargetRegistry::lookupTarget(targetTriple, error);
-  if (!target) {
-    llvm::errs() << error;
-    return 1;
+  fs::path inputFilePath = argv[1];
+  if (!fs::exists(inputFilePath)) {
+    std::cerr << "File " << inputFilePath << " does not exist" << std::endl;
+    std::exit(1);
   }
-  auto targetMachine = target->createTargetMachine(
-      targetTriple, "generic", "", llvm::TargetOptions(),
-      llvm::Optional<llvm::Reloc::Model>());
-  llvmModule.setDataLayout(targetMachine->createDataLayout());
+  std::string inputCode = readFileIntoString(inputFilePath);
 
-  // prepare object file
-  // TODO: temporaty dir
-  auto objFilename = "/tmp/unzig_object_file";
-  std::error_code EC;
-  llvm::raw_fd_ostream dest("/tmp/unzig_object_file", EC,
-                            llvm::sys::fs::OF_None);
-  if (EC) {
-    llvm::errs() << "Could not open file: " << EC.message();
-    return 1;
+  std::string llFileName = inputFilePath.filename().replace_extension("ll");
+  std::string objFileName = inputFilePath.filename().replace_extension(".o");
+  std::string outputFile = inputFilePath.filename().replace_extension();
+
+  // prepare .ll file
+  std::error_code OutErrorInfo;
+  std::error_code ok;
+  llvm::raw_fd_ostream llFile(llFileName, OutErrorInfo, llvm::sys::fs::OF_None);
+
+  if (OutErrorInfo != ok) {
+    std::cerr << "Unable to create .ll file" << std::endl;
+    std::exit(1);
   }
 
-  llvm::legacy::PassManager pass;
-  auto fileType = llvm::CGFT_ObjectFile;
-  if (targetMachine->addPassesToEmitFile(pass, dest, nullptr, fileType)) {
-    llvm::errs() << "targetMachine can't emit a file of this type";
-    return 1;
+  auto tokens = tokenize(inputCode);
+  auto ast = parse(std::move(tokens));
+  if (ast) {
+    CodeGenerator generator;
+    if (auto *code = ast->codegen(&generator)) {
+      code->print(llFile);
+    }
   }
-
-  pass.run(llvmModule);
-  dest.flush();
+  // compile to object file
+  auto llcCmd = std::string("llc -filetype=obj ") + llFileName;
+  (void)std::system(llcCmd.c_str());
 
   // linker
-  std::string outputFile = inputFile.filename().replace_extension();
   std::string linkerCmd =
       std::string("ld -o ") + outputFile +
-      " -dynamic-linker /lib64/ld-linux-x86-64.so.2 /usr/lib/crt1.o -lc " +
-      objFilename;
+      " -dynamic-linker /lib64/ld-linux-x86-64.so.2 /usr/lib/crt1.o -lc " + objFileName;
   return std::system(linkerCmd.c_str());
 }
