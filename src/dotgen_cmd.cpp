76db9e2646555a18bec3bf2742ab0bb36399cf2b
Roman Sokolkov <roman.sokolkov@apex.ai>
2022-06-05 20:52:54 +0200

    Add pipe

--- src/dotgen_cmd.cpp
+++ src/dotgen_cmd.cpp
@@ -1,23 +1,17 @@
-#include <filesystem>
 #include <iostream>
-#include <fstream>
-#include <readline/history.h>
-#include <readline/readline.h>
 
-#include "codegen.hpp"
 #include "dotgen.hpp"
 #include "parser.hpp"
 #include "tokenizer.hpp"
 
-namespace fs = std::filesystem;
-
 AstNodePtr parseExpr(ParserCtxt &ctxt);
+AstNodePtr parseRoot(ParserCtxt &ctxt);
 
 void generateDot(const std::string &input) {
   auto tokens = tokenize(input);
 
   ParserCtxt ctxt(std::move(tokens));
-  auto ast = parseExpr(ctxt);
+  auto ast = parseRoot(ctxt);
   if (ast) {
     std::string output;
     DotGenerator g;
@@ -32,27 +26,8 @@ void generateDot(const std::string &input) {
   }
 }
 
-
-std::string readFileIntoString(const std::string &path) {
-  std::ifstream input_file(path);
-  if (!input_file.is_open()) {
-    std::cerr << "Could not open the file - '" << path << "'" << std::endl;
-    std::exit(EXIT_FAILURE);
-  }
-  return std::string((std::istreambuf_iterator<char>(input_file)),
-                     std::istreambuf_iterator<char>());
-}
-
 int main(int argc, char **argv) {
-  if (argc != 2) {
-    std::cerr << "Usage: unzig_dotgen <file>.uz" << std::endl;
-    std::exit(1);
-  }
-  fs::path inputFilePath = argv[1];
-  if (!fs::exists(inputFilePath)) {
-    std::cerr << "File " << inputFilePath << " does not exist" << std::endl;
-    std::exit(1);
-  }
-  std::string inputCode = readFileIntoString(inputFilePath);
-  generateDot(inputCode);
+  std::istreambuf_iterator<char> begin(std::cin), end;
+  std::string input(begin, end);
+  generateDot(input);
 }
