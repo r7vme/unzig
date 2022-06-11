e11f6ffc94c7f719cba7ee68b31daf2b6573de6a
Roman Sokolkov <roman.sokolkov@apex.ai>
2022-06-11 15:37:03 +0200

    Working AST dot generation

--- src/dotgen.cpp
+++ src/dotgen.cpp
@@ -23,8 +23,7 @@ void DotGenerator::generate(IntegerExprNode *astNode, std::string &output) {
 
 void DotGenerator::generate(BinExprNode *astNode, std::string &output) {
   std::string binOpType;
-  switch (astNode->type)
-  {
+  switch (astNode->type) {
   case BinOpType::ADD:
     binOpType = "+";
     break;
@@ -52,11 +51,80 @@ void DotGenerator::generate(BinExprNode *astNode, std::string &output) {
   output.append(nodeId + "->" + getNodeId(astNode->rhs.get()) + "\n");
 }
 
-void DotGenerator::generate(VarDeclNode *astNode, std::string &output) {}
-void DotGenerator::generate(FnDefNode *astNode, std::string &output) {}
-void DotGenerator::generate(BlockNode *astNode, std::string &output) {}
-void DotGenerator::generate(RootNode *astNode, std::string &output) {}
-void DotGenerator::generate(AssignStNode *astNode, std::string &output) {}
-void DotGenerator::generate(ReturnStNode *astNode, std::string &output) {}
-void DotGenerator::generate(VarExprNode *astNode, std::string &output) {}
-void DotGenerator::generate(FnCallExprNode *astNode, std::string &output) {}
+void DotGenerator::generate(VarDeclNode *astNode, std::string &output) {
+  const auto text = std::string("VarDecl\\n") + astNode->name;
+  const auto nodeId = getNodeId(astNode);
+  output.append(nodeId + "[label=\"" + text + "\"]\n");
+
+  if (astNode->initExpr) {
+    astNode->initExpr->dotgen(this, output);
+    output.append(nodeId + "->" + getNodeId(astNode->initExpr.get()) + "\n");
+  }
+}
+
+void DotGenerator::generate(FnDefNode *astNode, std::string &output) {
+  const auto text = std::string("FnDef\\n") + astNode->name;
+  const auto nodeId = getNodeId(astNode);
+  output.append(nodeId + "[label=\"" + text + "\"]\n");
+
+  astNode->body->dotgen(this, output);
+  output.append(nodeId + "->" + getNodeId(astNode->body.get()) + "\n");
+}
+
+void DotGenerator::generate(BlockNode *astNode, std::string &output) {
+  const auto text = std::string("Block");
+  const auto nodeId = getNodeId(astNode);
+  output.append(nodeId + "[label=\"" + text + "\"]\n");
+
+  for (auto &d : astNode->statements) {
+    d->dotgen(this, output);
+    output.append(nodeId + "->" + getNodeId(d.get()) + "\n");
+  }
+}
+
+void DotGenerator::generate(RootNode *astNode, std::string &output) {
+  const auto text = std::string("Root");
+  const auto nodeId = getNodeId(astNode);
+  output.append(nodeId + "[label=\"" + text + "\"]\n");
+
+  for (auto &d : astNode->declarations) {
+    d->dotgen(this, output);
+    output.append(nodeId + "->" + getNodeId(d.get()) + "\n");
+  }
+}
+void DotGenerator::generate(AssignStNode *astNode, std::string &output) {
+  const auto text = std::string("AssignSt");
+  const auto nodeId = getNodeId(astNode);
+  output.append(nodeId + "[label=\"" + text + "\"]\n");
+
+  // lhs
+  astNode->lhs->dotgen(this, output);
+  output.append(nodeId + "->" + getNodeId(astNode->lhs.get()) + "\n");
+
+  // rhs
+  astNode->rhs->dotgen(this, output);
+  output.append(nodeId + "->" + getNodeId(astNode->rhs.get()) + "\n");
+}
+
+void DotGenerator::generate(ReturnStNode *astNode, std::string &output) {
+  const auto text = std::string("ReturnSt");
+  const auto nodeId = getNodeId(astNode);
+  output.append(nodeId + "[label=\"" + text + "\"]\n");
+
+  if (astNode->expr) {
+    astNode->expr->dotgen(this, output);
+    output.append(nodeId + "->" + getNodeId(astNode->expr.get()) + "\n");
+  }
+}
+
+void DotGenerator::generate(VarExprNode *astNode, std::string &output) {
+  const auto text = std::string("VarExpr\\n") + astNode->name;
+  const auto nodeId = getNodeId(astNode);
+  output.append(nodeId + "[label=\"" + text + "\"]\n");
+}
+
+void DotGenerator::generate(FnCallExprNode *astNode, std::string &output) {
+  const auto text = std::string("FnCallExpr\\n") + astNode->callee;
+  const auto nodeId = getNodeId(astNode);
+  output.append(nodeId + "[label=\"" + text + "\"]\n");
+}
