c9b284eeb18a850cf26b7122ca96f0060f60250d
Roman Sokolkov <roman.sokolkov@apex.ai>
2022-06-12 15:47:18 +0200

    SemanticAnalyzer skeleton

--- src/semantic_analysis.hpp
+++
@@ -1,3 +0,0 @@
-// Create scoped symbol tables
-// Fills AST with scoped symbol tables
-// Do semantic checks (e.g. resolve symbols)
---
+++ src/semantic_analyzer.hpp
@@ -0,0 +1,34 @@
+#pragma once
+// Create scoped symbol tables
+// Fills AST with scoped symbol tables
+// Do semantic checks (e.g. resolve symbols)
+
+class FloatExprNode;
+class IntegerExprNode;
+class BinExprNode;
+class VarDeclNode;
+class FnDefNode;
+class BlockNode;
+class RootNode;
+class AssignStNode;
+class ReturnStNode;
+class VarExprNode;
+class FnCallExprNode;
+
+class SemanticAnalyzer {
+
+public:
+  SemanticAnalyzer();
+
+  void *generate(FloatExprNode *astNode);
+  void *generate(IntegerExprNode *astNode);
+  void *generate(BinExprNode *astNode);
+  void *generate(VarDeclNode *astNode);
+  void *generate(FnDefNode *astNode);
+  void *generate(BlockNode *astNode);
+  void *generate(RootNode *astNode);
+  void *generate(AssignStNode *astNode);
+  void *generate(ReturnStNode *astNode);
+  void *generate(VarExprNode *astNode);
+  void *generate(FnCallExprNode *astNode);
+};
