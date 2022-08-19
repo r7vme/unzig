05e19bbf839c9967811ba639046c866301b2c9f7
Roman Sokolkov <rsokolkov@gmail.com>
2022-08-19 10:56:38 +0200

    Add helper script for generating headers

---
+++ codegen/generate.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+from jinja2 import Environment, FileSystemLoader
+
+TEMPLATE_DIR = 'codegen'
+
+files = [
+    'codegen.hpp',
+    'sema.hpp',
+    'dotgen.hpp',
+]
+
+ast_nodes = [
+    'FloatExprNode',
+    'IntegerExprNode',
+    'BoolExprNode',
+    'PrefixExprNode',
+    'BinExprNode',
+    'VarDeclNode',
+    'FnDefNode',
+    'BlockNode',
+    'RootNode',
+    'AssignStNode',
+    'ReturnStNode',
+    'IfStNode',
+    'VarExprNode',
+    'FnCallExprNode',
+    'EmptyNode',
+]
+
+
+if not os.path.exists(TEMPLATE_DIR):
+    sys.exit("template dir %s does not exist" % TEMPLATE_DIR)
+
+if not os.path.exists('src'):
+    sys.exit("src does not exist")
+
+file_loader = FileSystemLoader(TEMPLATE_DIR)
+env = Environment(loader=file_loader)
+env.keep_trailing_newline = True
+
+for f in files:
+    env.get_template(f + '.j2').stream(ast_nodes=ast_nodes).dump('src/' + f)
