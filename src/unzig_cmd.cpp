27605d42de506eba4de0edb3656fac4a9263514b
Roman Sokolkov <rsokolkov@gmail.com>
2024-08-05 20:01:52 +0300

    Fix missing header and update Catch2

--- src/unzig_cmd.cpp
+++ src/unzig_cmd.cpp
@@ -14,7 +14,7 @@
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Type.h"
 #include "llvm/Support/FileSystem.h"
-#include "llvm/Support/Host.h"
+#include "llvm/TargetParser/Host.h"
 #include "llvm/Support/TargetSelect.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Target/TargetMachine.h"
